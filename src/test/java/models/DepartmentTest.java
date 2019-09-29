7715a48679945bdfd4f42e3eed1ea82a12073e76
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-29 22:41:42 +0200

    added test for department class

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -18,17 +18,53 @@ public class DepartmentTest {
 
     @Test
     public void getName() {
+        Department testDepartment = setupDepartment();
+        assertEquals("Human Resources", testDepartment.getName());
+    }
+
+    @Test
+    public void setName() {
+        Department testDepartment= setupDepartment();
+        testDepartment.setName("job");
+        assertNotEquals("Human Resources",  testDepartment.getName());
+    }
+
+    public Department setupDepartment() {
+        return new Department("Human Resources","looking for a secretary",30);
     }
 
     @Test
     public void getDescription() {
+        Department testDepartment=setupDepartment();
+        assertEquals("looking for a secretary",testDepartment.getDescription());
+    }
+    @Test
+    public void setDescription() {
+        Department testDepartment= setupDepartment();
+        testDepartment.setDescription("job search");
+        assertNotEquals("looking for a secretary",  testDepartment.getDescription());
     }
 
     @Test
     public void getNumberOfEmployees() {
+        Department testDepartment=setupDepartment();
+        assertEquals(30,testDepartment.getNumberOfEmployees());
     }
-
     @Test
+    public void setNumberOfEmployees() {
+        Department testDepartment= setupDepartment();
+        testDepartment.setNumberOfEmployees(30);
+        assertNotEquals(38,  testDepartment.getNumberOfEmployees());
+    }
+ /*   @Test
     public void getId() {
+        Department testDepartment=setupDepartment();
+        assertEquals(2,testDepartment.getId());
+    }*/
+    @Test
+    public void setId() {
+        Department testDepartment= setupDepartment();
+        testDepartment.setId(2);
+        assertNotEquals(0,  testDepartment.getId());
     }
 }
