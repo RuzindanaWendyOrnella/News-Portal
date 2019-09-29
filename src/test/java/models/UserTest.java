6b9c081bc52d2036014e46523376c289944b249d
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-29 22:42:16 +0200

    added test for user class

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -18,12 +18,30 @@ public class UserTest {
 
     @Test
     public void getName() {
+      User testUser = setupUser();
+        assertEquals("wendy", testUser.getName());
+    }
+
+   public User setupUser() {
+        return new User("wendy","manager","leader");
     }
 
     @Test
     public void getPositionInCompany() {
+        User testUser = setupUser();
+        assertEquals("manager", testUser.getPositionInCompany());
     }
-
+    @Test
+    public void getRole() {
+        User testUser = setupUser();
+        assertEquals("leader", testUser.getRole());
+    }
+    @Test
+    public void setRole() {
+        User testUser= setupUser();
+        testUser.setRole("leader");
+        assertNotEquals("boss",  testUser.getRole());
+    }
     @Test
     public void getId() {
     }
