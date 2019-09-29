a89c5878832bf77d2739f51862739f5662326b72
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-29 22:38:51 +0200

    updated user class with getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -3,25 +3,41 @@ package models;
 public class User {
     private String name;
     private String positionInCompany;
-    private int departmentId;
+ /*   private int departmentId;*/
     private String role;
     private int id;
 
-    public User(String name,String positionInCompany,int departmentId,String role){
+    public User(String name,String positionInCompany,String role){
         this.name=name;
         this.positionInCompany=positionInCompany;
-        this.departmentId=departmentId;
+
     }
     public String getName(){
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
+    }
     public String getPositionInCompany(){
         return positionInCompany;
     }
-    public int getDepartmentId(){
-        return departmentId;
+    public void setPositionInCompany(String positionInCompany) {
+        this.positionInCompany = positionInCompany;
     }
+  /*  public int getDepartmentId(){
+        return departmentId;
+    }*/
     public int getId(){
         return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 }
