0fe3be292735560b298eb31ef253cbeeaedd0284
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-29 20:51:04 +0200

    created class user

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,27 @@
 package models;
 
 public class User {
+    private String name;
+    private String positionInCompany;
+    private int departmentId;
+    private String role;
+    private int id;
+
+    public User(String name,String positionInCompany,int departmentId,String role){
+        this.name=name;
+        this.positionInCompany=positionInCompany;
+        this.departmentId=departmentId;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPositionInCompany(){
+        return positionInCompany;
+    }
+    public int getDepartmentId(){
+        return departmentId;
+    }
+    public int getId(){
+        return id;
+    }
 }
