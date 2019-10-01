4a2be10dd4fdc1500d65a8b655c99b1c2621bdbd
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-10-01 19:21:58 +0200

    created class user

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,16 +1,22 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.util.Objects;
+
 public class User {
     private String name;
-    private String positionInCompany;
+    private String positionincompany;
  /*   private int departmentId;*/
     private String role;
     private int id;
+    private int departmentid;
 
-    public User(String name,String positionInCompany,String role){
+    public User(String name,String positionincompany,String role ){
         this.name=name;
-        this.positionInCompany=positionInCompany;
-
+        this.positionincompany=positionincompany;
+        this.role=role;
+        this.departmentid=departmentid;
     }
     public String getName(){
         return name;
@@ -22,22 +28,63 @@ public class User {
     public String getRole() {
         return role;
     }
-    public void setRole(String role){
-        this.role=role;
+
+    public void setRole(String role) {
+        this.role = role;
     }
-    public String getPositionInCompany(){
-        return positionInCompany;
+
+    public int getDepartmentid() {
+        return departmentid;
     }
-    public void setPositionInCompany(String positionInCompany) {
-        this.positionInCompany = positionInCompany;
+
+    public String getPositionincompany() {
+        return positionincompany;
     }
-  /*  public int getDepartmentId(){
-        return departmentId;
-    }*/
+
+    public void setPositionincompany(String positionincompany) {
+        this.positionincompany = positionincompany;
+    }
+
+    /*  public int getDepartmentId(){
+            return departmentId;
+        }*/
     public int getId(){
         return id;
     }
     public void setId(int id) {
         this.id = id;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+       User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(positionincompany, user.positionincompany) &&
+                Objects.equals(role, user.role) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, positionincompany,role,id);
+    }
+
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO userincompany (name,positionincompany,role,departmentid) VALUES (:name,:positionincompany,:role,:departmentid);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("positionincompany", this.positionincompany)
+                    .addParameter("role", this.role)
+                    .addParameter("departmentid", this.departmentid)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 }
