b5a501b86c7192eecc4d8ee7f29faf5737d54512
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-10-01 19:10:03 +0200

    updated interface dao by commenting out getAll()

--- src/main/java/Dao/DepartmentDao.java
+++ src/main/java/Dao/DepartmentDao.java
@@ -1,4 +1,27 @@
 package Dao;
 
+
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    void add(Department department);
+    void addDepartmentForUser(Department department, User user);
+
+    //read
+   /* List<Department> getAll();*/
+    List<User> getAllUsersForDepartment(int DepartmentId);
+
+    //update
+    //omit for now
+    void update(int id, String name, String description, int numberOfEmployees);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
+  Department findById(int id);
+    List<User> getAllDepartmentByUser(int DepartmentId);
 }
