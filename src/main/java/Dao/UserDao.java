8d9230affeef51477173326b67de4c3b8426ca94
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-10-01 19:14:57 +0200

    created and updated interface userdao

--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,4 +1,23 @@
 package Dao;
 
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    void add(User user);
+    void addUserToDepartment(User user, Department department);
+
+//    List<User> getAll();
+    List<Department> getAllDepartmentsForUsers(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
+    User findById(int id);
 }
